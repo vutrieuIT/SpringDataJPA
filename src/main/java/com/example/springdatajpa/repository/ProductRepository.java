c5b50bb454907fd0a7491e551a2c76a5cdc8eaee
Trieu Vu <vutrieu2002@gmail.com>
2023-07-24 16:25:54 +0700

    Spring Data JPA: Query method

--- src/main/java/com/example/springdatajpa/repository/ProductRepository.java
+++ src/main/java/com/example/springdatajpa/repository/ProductRepository.java
@@ -3,5 +3,13 @@ package com.example.springdatajpa.repository;
 import com.example.springdatajpa.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByNameContaining(String keyword);
+
+    List<Product> findByPriceGreaterThan(Double price);
+
+    List<Product> findFirst5ByOrderByPriceAsc();
 }
