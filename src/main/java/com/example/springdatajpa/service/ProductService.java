ff0b7e03e23957d97ed9a689011e4b3c22921b73
Trieu Vu <vutrieu2002@gmail.com>
2023-07-24 16:03:52 +0700

    Spring Data JPA: create Product service

--- src/main/java/com/example/springdatajpa/service/ProductService.java
+++ src/main/java/com/example/springdatajpa/service/ProductService.java
@@ -1,2 +1,12 @@
-package com.example.springdatajpa.service;public interface IProductService {
+package com.example.springdatajpa.service;
+
+import com.example.springdatajpa.model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    public List<Product> findAll();
+    public Product save(Product product);
+    public void delete(Long Id);
+    public Product findById(Long id);
 }
--- src/main/java/com/example/springdatajpa/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/springdatajpa/service/impl/ProductServiceImpl.java
@@ -1,2 +1,35 @@
-package com.example.springdatajpa.service.impl;public class ProductServiceImpl {
+package com.example.springdatajpa.service.impl;
+
+import com.example.springdatajpa.model.Product;
+import com.example.springdatajpa.repository.ProductRepository;
+import com.example.springdatajpa.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+    @Override
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public void delete(Long Id) {
+        productRepository.deleteById(Id);
+    }
+
+    @Override
+    public Product findById(Long Id) {
+        return productRepository.findById(Id).orElse(null);
+    }
 }
