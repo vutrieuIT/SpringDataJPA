dbd741bb596e2a73b68abb7888793cad8d344052
Trieu Vu <vutrieu2002@gmail.com>
2023-07-24 16:03:17 +0700

    Spring Data JPA: create Product Repository

--- src/main/java/com/example/springdatajpa/repository/ProductRepository.java
+++ src/main/java/com/example/springdatajpa/repository/ProductRepository.java
@@ -1,2 +1,7 @@
-package com.example.springdatajpa.repository;public interface ProductRepository {
+package com.example.springdatajpa.repository;
+
+import com.example.springdatajpa.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
